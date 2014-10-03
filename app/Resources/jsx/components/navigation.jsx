317d3a1b3657c64e1be669d3c0c4d74940c665f7
Robert Schönthal <caziel@gmx.net>
2014-10-03 13:32:58 +0200

    mobile layout fixes

--- app/Resources/jsx/components/navigation.jsx
+++ app/Resources/jsx/components/navigation.jsx
@@ -6,24 +6,36 @@ var FilterBar = require('./filter-bar.jsx');
 var NavControls = require('./nav-controls.jsx');
 
 var Navigation = React.createClass({
-//TODO port toggling
+
+    getInitialState: function () {
+        return {
+            collapsed: true
+        };
+    },
+
+    toggle: function() {
+        this.setState({collapsed: !this.state.collapsed});
+    },
+
     render: function () {
+        var classes = this.state.collapsed ? 'navbar-collapse collapse' : 'navbar-collapse collapse in';
+
         return (
             <div className="top-navigation" role="navigation">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed">
+                        <button type="button" ref="toggler" className="navbar-toggle" onClick={this.toggle}>
                             <span className="sr-only">Toggle navigation</span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                         </button>
-                        <a className="navbar-brand" href="#">Issue Dashboard</a>
+                        <a className="navbar-brand" href="#">Issues</a>
+
+                        <NavControls collapsedFilters={this.props.collapsedFilters} collapsed={this.props.collapsed} filters={this.props.filters} />
                     </div>
 
-                    <NavControls collapsedFilters={this.props.collapsedFilters} collapsed={this.props.collapsed} filters={this.props.filters} />
-
-                    <div className="navbar-collapse collapse">
+                    <div className={classes}>
                         <FilterBar collapsedFilters={this.props.collapsedFilters} issues={this.props.issues} filters={this.props.filters} />
                     </div>
                 </div>
